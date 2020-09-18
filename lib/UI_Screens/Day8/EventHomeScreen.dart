120724c020051fb2dab456472eb44fb1a80c06ce
Del Ferns <fernandesdelicia07@gmail.com>
2020-09-19 01:21:20 +0530

    Day 5 almost done

--- lib/UI_Screens/Day8/EventHomeScreen.dart
+++ lib/UI_Screens/Day8/EventHomeScreen.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'Event.dart';
 import 'package:flappy_search_bar/flappy_search_bar.dart';
+
+final EventDetails _details = EventDetails();
 
 class EventHomeScreen extends StatelessWidget {
   static String id = '/EventHomeScreen';
@@ -18,7 +20,7 @@ class EventHomeScreen extends StatelessWidget {
           Container(
             width: 50.0,
             height: 10.0,
-            margin: EdgeInsets.only(right: 10.0),
+            margin: EdgeInsets.only(right: 10.0, bottom: 10.0),
             decoration: BoxDecoration(
               image: DecorationImage(
                   fit: BoxFit.cover,
@@ -30,8 +32,7 @@ class EventHomeScreen extends StatelessWidget {
         ],
       ),
       body: Padding(
-        padding:
-            EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0, bottom: 40.0),
+        padding: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 40.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
@@ -40,22 +41,124 @@ class EventHomeScreen extends StatelessWidget {
             //   onItemFound: (item, int index) {},
             // ),
             Expanded(
-              child: ListView(
+              child: ListView.builder(
                 shrinkWrap: true,
                 scrollDirection: Axis.vertical,
-                children: [
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    height: 220.0,
-                    decoration: BoxDecoration(
-                      color: Colors.black54,
-                    ),
-                  ),
-                ],
+                physics: AlwaysScrollableScrollPhysics(),
+                itemCount: _details.getLength(),
+                itemBuilder: (context, int index) {
+                  return EventListTile(eventNumber: index);
+                },
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class EventListTile extends StatelessWidget {
+  final int eventNumber;
+  EventListTile({this.eventNumber});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(top: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            children: [
+              Text(
+                _details.getDay(eventNumber),
+                style: TextStyle(
+                  color: Color(0xff40A2F3),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40.0,
+                ),
+              ),
+              Text(
+                _details.getMonth(eventNumber),
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                ),
+              ),
+            ],
+          ),
+          Container(
+            width: MediaQuery.of(context).size.width / 1.4,
+            height: 220.0,
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(
+                  _details.getImageUrl(eventNumber),
+                ),
+                fit: BoxFit.cover,
+              ),
+              borderRadius: BorderRadius.all(
+                Radius.circular(25.0),
+              ),
+            ),
+            child: Container(
+              width: MediaQuery.of(context).size.width / 1.4,
+              padding: EdgeInsets.only(left: 20.0, bottom: 20.0),
+              height: 220.0,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.bottomRight,
+                  stops: [0.5, 1],
+                  colors: [
+                    Colors.black.withOpacity(.9),
+                    Colors.black.withOpacity(.3)
+                  ],
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(25.0),
+                ),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    '${_details.getName(eventNumber)} ${_details.getYear(eventNumber)}',
+                    softWrap: true,
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30.0,
+                    ),
+                  ),
+                  SizedBox(height: 5.0),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.access_time_outlined,
+                        color: Colors.white,
+                        size: 20.0,
+                      ),
+                      SizedBox(width: 5.0),
+                      Text(
+                        _details.getTime(eventNumber),
+                        softWrap: true,
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 15.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
